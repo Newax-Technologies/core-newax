1114a066dab12d28f0f182f07200771c5c815e8d
Dr. Nadeem Murtaza <254184253+nadeemmurtaza@users.noreply.github.com>
2026-07-10 20:38:43 +0500

    Register global API configuration

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { HealthController } from './health/health.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      envFilePath: ['.env.local', '.env'],
+    }),
+  ],
   controllers: [HealthController],
   providers: [],
 })
