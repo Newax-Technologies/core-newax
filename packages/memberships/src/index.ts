76e51dc09aa3e6029ba4f377ab5643edc852710f
Dr. Nadeem Murtaza <254184253+nadeemmurtaza@users.noreply.github.com>
2026-07-11 23:04:48 +0500

    Export memberships registry module API

---
+++ packages/memberships/src/index.ts
@@ -0,0 +1,31 @@
+export type { MembershipsRepository } from './database/memberships-repository';
+export type {
+  MembershipEvent,
+  MembershipEventName,
+  MembershipEventPublisher,
+} from './events/membership-event';
+export {
+  MembershipModuleError,
+  type MembershipErrorCode,
+} from './errors/membership-module-error';
+export {
+  MEMBERSHIP_PERMISSIONS,
+  type MembershipPermission,
+} from './permissions/membership-permissions';
+export type { MembershipReferenceDirectory } from './services/membership-reference-directory';
+export { MembershipsService } from './services/memberships.service';
+export type {
+  CreateMembershipInput,
+  CreateMembershipRecordInput,
+  CreateMembershipResult,
+  MembershipListQuery,
+  MembershipPage,
+  MembershipRecord,
+  MembershipReferenceRecord,
+  MembershipReferenceStatus,
+  MembershipRequestContext,
+  MembershipStatus,
+  MutableMembershipStatus,
+  UpdateMembershipInput,
+  UpdateMembershipRecordInput,
+} from './types/membership';
