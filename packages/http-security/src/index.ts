8575614745d4ba1335568b0ab7cfbdacc1af9755
Dr. Nadeem Murtaza <254184253+nadeemmurtaza@users.noreply.github.com>
2026-07-12 02:03:21 +0500

    Export HTTP security boundary API

---
+++ packages/http-security/src/index.ts
@@ -0,0 +1,30 @@
+export {
+  HttpSecurityError,
+  type HttpSecurityErrorCode,
+} from './errors/http-security-error';
+export { CookieHeaderParser } from './services/cookie-header-parser';
+export { HttpRateLimiter } from './services/http-rate-limiter';
+export type {
+  HttpRateLimitStore,
+  HttpSecurityAuditSink,
+  HttpSecurityClock,
+  HttpSecurityCrypto,
+} from './services/http-security-ports';
+export { RequestOriginPolicy } from './services/request-origin-policy';
+export { SecureCookieTransport } from './services/secure-cookie-transport';
+export { SecurityHeadersPolicy } from './services/security-headers-policy';
+export { SensitiveResponseRedactor } from './services/sensitive-response-redactor';
+export { SignedCsrfTokenService } from './services/signed-csrf-token.service';
+export type {
+  CsrfValidationInput,
+  HttpAuditRecord,
+  HttpCookieValues,
+  HttpRateLimitInput,
+  HttpRateLimitResult,
+  HttpSecurityContextMode,
+  HttpSecurityHeaders,
+  HttpSecurityMethod,
+  HttpSecurityPolicy,
+  HttpSecurityRequest,
+  IssuedCsrfToken,
+} from './types/http-security';
