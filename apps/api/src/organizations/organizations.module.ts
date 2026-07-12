368a9099650837967e34af6bc72b4fbf745a9efa
Dr. Nadeem Murtaza <254184253+nadeemmurtaza@users.noreply.github.com>
2026-07-12 14:03:21 +0500

    Compose current organization controller

--- apps/api/src/organizations/organizations.module.ts
+++ apps/api/src/organizations/organizations.module.ts
@@ -2,11 +2,14 @@ import { Module } from '@nestjs/common';
 import { OrganizationsService } from '@newax/organizations';
 
 import { DatabaseModule } from '../database/database.module';
+import { RequestContextModule } from '../request-context/request-context.module';
+import { CurrentOrganizationController } from './current-organization.controller';
 import { LoggingOrganizationEventPublisher } from './logging-organization-event.publisher';
 import { PrismaOrganizationRepository } from './prisma-organization.repository';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, RequestContextModule],
+  controllers: [CurrentOrganizationController],
   providers: [
     PrismaOrganizationRepository,
     LoggingOrganizationEventPublisher,
@@ -16,7 +19,8 @@ import { PrismaOrganizationRepository } from './prisma-organization.repository';
       useFactory: (
         repository: PrismaOrganizationRepository,
         eventPublisher: LoggingOrganizationEventPublisher,
-      ): OrganizationsService => new OrganizationsService(repository, eventPublisher),
+      ): OrganizationsService =>
+        new OrganizationsService(repository, eventPublisher),
     },
   ],
   exports: [OrganizationsService],
